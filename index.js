4ffcfa2eb5818faa2275a9ffe8f562a984aa0817
Manmohan Batra <53882458+manmohanbatra@users.noreply.github.com>
2023-02-05 14:32:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function mooncakeSays(message) {
-  let mooncake = '(0.o)';
+  let mooncake = '(o.o)';
 
   if ( !message ) {
     return `${mooncake} chookity?`;
   }
 
-  return `${mooncake} ${message}`!;
+  return `${mooncake} ${message}`;
 }
 
 module.exports.mooncakeSays = mooncakeSays;
